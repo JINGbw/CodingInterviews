0206a32075c4aa0e87b27b12421081bc15f14c08
Leetcode <43645876+JINGbw@users.noreply.github.com>
2019-08-03 19:03:05 +0800

    Update 构建乘积数组.cpp

--- 数组/构建乘积数组.cpp
+++ 数组/构建乘积数组.cpp
@@ -3,3 +3,18 @@
 //思路：循环两遍数组
 //1. 第一趟从前往后 计算 i= 1,2,...n-1 令B[i]=B[i-1]A[i-1]
 //2. 第二趟从后往前 计算 i= n-2,n-1...1 令B[i] = B[i]A[i+1]...A[n-1]
+class Solution {
+public:
+    vector<int> multiply(const vector<int>& A) {
+        vector<int>B(A.size(),1);
+        for(int i = 1; i<A.size(); i++){
+            B[i] = B[i-1]*A[i-1];
+        }
+        int tmp=1;
+        for(int i = A.size()-2;i>=0;i--){
+            tmp*=A[i+1];
+            B[i] *= tmp;
+        }
+        return B;
+    }
+};
