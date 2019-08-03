90a02604ecb05f8a5c046c9fa9db74c76dd89d04
Leetcode <43645876+JINGbw@users.noreply.github.com>
2019-08-03 11:22:52 +0800

    Update 二维数组的查找.cpp

--- 数组/二维数组的查找.cpp
+++ 数组/二维数组的查找.cpp
@@ -1,2 +1,21 @@
 //法一：利用二分法找到该数所在的行，再用二分法找到这个数
 //法二：从左下角或者右上角开始找，可以根据Target与当前元素的大小关系来缩小查找区间
+
+    bool Find(int target, vector<vector<int> > array) {
+        int rows = array.size();
+        int cols = array[0].size();
+        if (rows==0||cols==0)
+            return false;
+        int x = 0,y = cols-1;
+        while ( x< rows && y>=0){
+            if(array[x][y]==target){
+                return true;
+            }
+            else if (array[x][y]>target){
+                y--;
+            }
+            else 
+                x++;
+        }
+        return false;
+    }
