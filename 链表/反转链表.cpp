fbcd0c253777f1ce0bb5d6da65ceb72ced431fc8
Leetcode <43645876+JINGbw@users.noreply.github.com>
2019-08-04 12:56:59 +0800

    Create 反转链表.cpp

---
+++ 链表/反转链表.cpp
@@ -0,0 +1,21 @@
+//使用三个指针，一个头指针进行反转 
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* pHead) {
+         ListNode* next = nullptr;
+         ListNode* newhead = nullptr;
+         ListNode* cur = pHead;
+         ListNode* pre = nullptr;
+        while(cur){
+            next = cur->next;
+            //循环终止条件：
+            if(!next){
+                newhead = cur; //注意这里不能退出，因为还要连接上之前的节点
+            }
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return newhead;
+    }
+};
